50e9330c39d1bb32fb0a7a264d02e8fea306d934
8area8 <mbriolet.ma@gmail.com>
2018-09-07 23:41:03 +0200

    trying to work with the ajax request.

--- grandpybot/__init__.py
+++ grandpybot/__init__.py
@@ -5,3 +5,5 @@ from flask import Flask
 
 app = Flask(__name__)
 # app.config.from_object('websiteconfig')
+
+from grandpybot import views
--- grandpybot/views.py
+++ grandpybot/views.py
@@ -1,6 +1,7 @@
 """Main file for views."""
 
-from grandpybot import app, render_template
+from grandpybot import app
+from flask import render_template, request
 
 
 @app.route('/')
@@ -14,3 +15,21 @@ def index():
 def not_found(error):
     """If 404 error."""
     return (render_template('404.html'), 404)
+
+
+
+@app.route('/about/')
+def about():
+    """Return the about file."""
+    return render_template('about.html')
+
+
+@app.route('/google_place_request', methods=['GET', 'POST'])
+def place_request():
+    """Send a request to the Google place API."""
+    req = request.args
+    import pdb; pdb.set_trace()
+    keywords = ""
+    key = "AIzaSyDZrTwZdIVBsCMN0YbA4C5La7Ytb8OIWkw"
+    url = ("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
+           f"input={keywords}&inputtype=textquery&fields=geometry&key={key}")
