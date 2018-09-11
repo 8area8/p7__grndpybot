ffe766818b6bed190af8cfa13621759dc8544051
8area8 <mbriolet.ma@gmail.com>
2018-09-11 15:18:09 +0200

    reworked config and envrionnment variables for production.

--- grandpybot/__init__.py
+++ grandpybot/__init__.py
@@ -1,9 +1,16 @@
 """Flask init."""
 
+import os
+
 from flask import Flask
 
 
 app = Flask(__name__)
-app.config.from_object('flaskenv')
+
+is_production = os.environ.get('IS_HEROKU', None)
+if is_production:
+    app.config["GOOGLE_KEY"] = os.environ.get('GOOGLE_KEY')
+else:
+    app.config.from_object('flaskenv')
 
 from grandpybot import views
